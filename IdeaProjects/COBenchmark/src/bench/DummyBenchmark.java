4f300f2a1bfa4f0c72167641594277f227354d69
lucas <lucasmis55@gmail.com>
2023-03-05 22:13:09 +0200

    Laptop Commit 3

--- IdeaProjects/COBenchmark/src/bench/DummyBenchmark.java
+++ IdeaProjects/COBenchmark/src/bench/DummyBenchmark.java
@@ -1,1 +1,30 @@
 package bench;
+
+public class DummyBenchmark implements iBenchmark
+{
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void run(Object... params) {
+
+    }
+
+    @Override
+    public void initialize(Object... params) {
+
+    }
+
+    @Override
+    public void clean() {
+
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+}
