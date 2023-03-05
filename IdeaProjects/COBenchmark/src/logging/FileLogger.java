72ad718fa0303160df7e56625a8bef594008a2d8
Lucas <lucasmis55@gmail.com>
2023-03-05 19:44:10 +0200

    PC Commit

--- IdeaProjects/COBenchmark/src/logging/FileLogger.java
+++ IdeaProjects/COBenchmark/src/logging/FileLogger.java
@@ -1,1 +1,25 @@
 package logging;
+
+public class FileLogger implements iLog
+{
+
+    @Override
+    public void write(long value) {
+
+    }
+
+    @Override
+    public void write(String string) {
+
+    }
+
+    @Override
+    public void write(Object... values) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
