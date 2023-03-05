2a13427f19e36290d75c2f2690ceded5bd360cd1
lucas <lucasmis55@gmail.com>
2023-03-05 15:55:15 +0200

    laptop commit

--- IdeaProjects/COBenchmark/src/logging/ConsoleLogger.java
+++ IdeaProjects/COBenchmark/src/logging/ConsoleLogger.java
@@ -1,1 +1,26 @@
 package logging;
+
+public class ConsoleLogger implements iLog
+{
+    @Override
+    public void write(long value) {
+        System.out.println(value);
+    }
+
+    @Override
+    public void write(String string) {
+        System.out.println(string);
+    }
+
+    @Override
+    public void write(Object... values) {
+        for(Object o:values)
+        {
+            System.out.println((Object)o.toString());
+        }
+    }
+    @Override
+    public void close() {
+
+    }
+}
