bfa6864bfd3508fe36b745241ed0abe24d09db20
lucas <lucasmis55@gmail.com>
2023-05-07 18:16:37 +0300

    CO Lab 7

---
+++ IdeaProjects/COBenchmark/src/testbench/TestCPUThreadedHashing.java
@@ -0,0 +1,30 @@
+package testbench;
+
+import bench.cpu.CPUThreadedHashing;
+import bench.cpu.CPUFixedPoint;
+import bench.iBenchmark;
+import logging.ConsoleLogger;
+import logging.TimeUnit;
+import logging.iLog;
+import timing.Timer;
+import timing.iTimer;
+public class TestCPUThreadedHashing {
+    public static void main(String[] args) {
+        iBenchmark bench = new CPUThreadedHashing();
+        int maxLength = 10;
+        int nThreads = 8;
+        int hashCode = 46053341;
+        TimeUnit Milisecond = TimeUnit.Mili;
+        TimeUnit Microsecond = TimeUnit.Micro;
+        TimeUnit Nanosecond = TimeUnit.Nano;
+        TimeUnit Second = TimeUnit.Sec;
+        iTimer timer = new Timer();
+        iLog log = new ConsoleLogger();
+        timer.start();
+
+        bench.run(maxLength, nThreads, hashCode);
+        long time = timer.stop();
+        log.writeTime("Finished in", time, Second);
+        log.write("Result is", bench.getResult());
+    }
+}
