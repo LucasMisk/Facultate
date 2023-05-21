d897b8b14854ffc955e7c3cc08ca3282ba8800bd
LucasMisk <lucasmis55@gmail.com>
2023-05-21 22:47:16 +0300

    Added Week 12 HW

--- IdeaProjects/COBenchmark/src/testbench/TestHDDRandomAccess.java
+++ IdeaProjects/COBenchmark/src/testbench/TestHDDRandomAccess.java
@@ -8,10 +8,10 @@ public class TestHDDRandomAccess {
     {
         iLog log = new ConsoleLogger();
         iBenchmark bench = new HDDRandomAccess();
-        long fileSize = 2 * 1024 * 1024 * 1024L; //2GB
-        int bufferSize = 4 * 1024; // 4KB
+        long fileSize = 1024 * 1024 * 1024L; //1GB
+        int bufferSize = 1024 * 1024; // 4KB
         bench.initialize(fileSize);
-        bench.run("r", "ft", bufferSize);
+        bench.run("r", "fs", bufferSize);
         log.write(bench.getResult());
         bench.clean();
         log.close();
